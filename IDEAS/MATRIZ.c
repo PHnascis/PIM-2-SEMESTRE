eb701a798563a9b8fa19805bf143e0ed85c9a88e
Pedrovisk NasciS <phnascis@gmail.com>
2024-10-10 09:46:58 -0300

    Update MATRIZ.c

--- IDEAS/MATRIZ.c
+++ IDEAS/MATRIZ.c
@@ -1,19 +1,21 @@
+#include <stdio.h>
+
 int main(){
 
 
-int mat[3][3];
+int mat[10][10];
 int i, j, soma;
 
-    for(i=0; i<3; i++){
-        for(j=0; j<3; j++){
-         mat[i][j] = (i *3) + j + 1;
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+         mat[i][j] = (i *10) + j + 1;
 
         }
     }
 
-     for(i=0; i<3; i++){
-        for(j=0; j<3; j++){
-        printf("%3d", mat[i][j]);
+     for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+        printf("%5d", mat[i][j]);
 
         }
         printf("\n");
